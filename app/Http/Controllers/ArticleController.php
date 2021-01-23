2d1e2fb4d7887e7d6757c9dcef9a6a127bcfcdf8
Ridwanur Khan <rahmankhan1607@gmail.com>
2021-01-23 20:57:00 +0600

    First API Resources using laravel 8 API

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Article;
+use App\Http\Resources\Article as ArticleResource;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //get all articles
+
+        $articleList = Article::paginate(15);
+
+        //return collection using resources
+
+       return ArticleResource::collection($articleList);
+    
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $article = Article::findOrFail($id);
+
+        return new ArticleResource($article);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+//article List
+
+Route::get('articles', [ArticleController::class, 'index']);
+
+//Single article
+
+Route::get('article/{id}', [ArticleController::class, 'show']);
+
+//create article
+Route::post('article', [ArticleController::class, 'store']);
+
+//update Article
+Route::put('articles', [ArticleController::class, 'store']);
+
+//delete Article
+Route::delete('articles', [ArticleController::class, 'destroy']);
